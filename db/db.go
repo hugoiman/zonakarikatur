d9823f39c44282d284eb0ea34a1331b6a50f6a95
hugoiman <hugogiman17@gmail.com>
2020-07-14 14:26:44 +0700

    db

--- db/db.go
+++ db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	// _ is import
 	_ "github.com/go-sql-driver/mysql"
@@ -10,14 +11,14 @@ import (
 
 // Connect is func
 func Connect() *sql.DB {
-	// username := os.Getenv("DB_USERNAME")
-	// password := os.Getenv("DB_PASSWORD")
-	// host := os.Getenv("DB_HOST")
-	// port := os.Getenv("DB_PORT")
-	// dbname := os.Getenv("DB_NAME")
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbname := os.Getenv("DB_NAME")
 
-	// db, err := sql.Open("mysql", username+":"+password+"@tcp("+host+":"+port+")/"+dbname+"?parseTime=true")
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/zonakarikatur?parseTime=true")
+	db, err := sql.Open("mysql", username+":"+password+"@tcp("+host+":"+port+")/"+dbname+"?parseTime=true")
+	// db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/zonakarikatur?parseTime=true")
 
 	if err != nil {
 		fmt.Println("db is not connected")
