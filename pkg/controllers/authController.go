3f91c5fc87da3159b7b9ea6acdf687f2037e1ee4
hugoiman <hugogiman17@gmail.com>
2020-07-22 21:42:11 +0700

    env email

--- pkg/controllers/authController.go
+++ pkg/controllers/authController.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
+	"os"
 	"time"
 	mw "zonakarikatur/middleware"
 	models "zonakarikatur/pkg/models"
@@ -140,8 +141,7 @@ func SendEmail(subject string, to string, message string) error {
 	var configSMTPHost = "smtp.gmail.com"
 	var configSMTPPort = 587
 	var configEmail = "nanonymoux@gmail.com"
-	var configPassword = "kudaponi10"
-	// var configPassword = os.Getenv("PASS_EMAIL")
+	var configPassword = os.Getenv("PASS_EMAIL")
 
 	mailer := gomail.NewMessage()
 
